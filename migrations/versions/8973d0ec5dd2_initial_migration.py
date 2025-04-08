ff9c8147460de29d459c12bd73af881c1b8ae3e9
sniper7jd <dhingra.jagrit15@gmail.com>
2025-04-08 11:53:24 -0400

    migrations fixed

--- migrations/versions/8973d0ec5dd2_initial_migration.py
+++ migrations/versions/8973d0ec5dd2_initial_migration.py
@@ -1,8 +1,8 @@
-"""empty message
+"""Initial migration
 
-Revision ID: 6ea56feef319
+Revision ID: 8973d0ec5dd2
 Revises: 
-Create Date: 2025-03-03 12:44:07.530143
+Create Date: 2025-04-08 11:48:19.691241
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '6ea56feef319'
+revision = '8973d0ec5dd2'
 down_revision = None
 branch_labels = None
 depends_on = None
@@ -31,11 +31,19 @@ def upgrade():
         batch_op.create_index(batch_op.f('ix_user_email'), ['email'], unique=True)
         batch_op.create_index(batch_op.f('ix_user_username'), ['username'], unique=True)
 
+    op.create_table('followers',
+    sa.Column('follower_id', sa.Integer(), nullable=False),
+    sa.Column('followed_id', sa.Integer(), nullable=False),
+    sa.ForeignKeyConstraint(['followed_id'], ['user.id'], ),
+    sa.ForeignKeyConstraint(['follower_id'], ['user.id'], ),
+    sa.PrimaryKeyConstraint('follower_id', 'followed_id')
+    )
     op.create_table('post',
     sa.Column('id', sa.Integer(), nullable=False),
     sa.Column('body', sa.String(length=140), nullable=False),
     sa.Column('timestamp', sa.DateTime(), nullable=False),
     sa.Column('user_id', sa.Integer(), nullable=False),
+    sa.Column('language', sa.String(length=5), nullable=True),
     sa.ForeignKeyConstraint(['user_id'], ['user.id'], ),
     sa.PrimaryKeyConstraint('id')
     )
@@ -53,6 +61,7 @@ def downgrade():
         batch_op.drop_index(batch_op.f('ix_post_timestamp'))
 
     op.drop_table('post')
+    op.drop_table('followers')
     with op.batch_alter_table('user', schema=None) as batch_op:
         batch_op.drop_index(batch_op.f('ix_user_username'))
         batch_op.drop_index(batch_op.f('ix_user_email'))
