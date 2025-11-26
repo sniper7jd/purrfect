574906224b4298bd817e48456fac6e043a15b9a7
sniper7jd <dhingra.jagrit15@gmail.com>
2025-11-26 01:17:58 -0500

    Add Render domain to ALLOWED_HOSTS

--- petconnect/petconnect/settings.py
+++ petconnect/petconnect/settings.py
@@ -13,7 +13,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-change-this-in-production')
 DEBUG = os.environ.get('DEBUG', 'True').lower() == 'true'
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = [
+    "petconnect-2gip.onrender.com",
+    ".onrender.com",
+    "localhost",
+    "127.0.0.1",
+]
 
 # Application definition
 INSTALLED_APPS = [
